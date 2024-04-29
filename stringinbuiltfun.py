ad9ccaec999ee76091a4be71276ba6f2c0549d12
Snithya02 <167056142+Snithya02@users.noreply.github.com>
2024-04-29 19:44:40 +0530

    Add files via upload

---
+++ stringinbuiltfun.py
@@ -0,0 +1,25 @@
+
+'''s="gsdfjghjh"
+print(s[-3])'''
+s="Python proGramming"
+print(s.upper())
+print(s.lower())
+s="4python"
+print(s.isupper())
+print(s.islower())
+print(s.isalpha())
+print(s.isalnum())
+print(s.isspace())
+s='4python programming'
+print(s.index('p'))
+print(s.rindex('p'))
+print(s.find('z'))
+print(s.rfind('p'))
+print(s.count('p'))
+s='4python programming'
+print(s.split())
+print(s.split('n'))
+print(s.partition('n'))
+a=['1','2','3']
+a=" ".join(a)
+print(a,type(a),a[0])
