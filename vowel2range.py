f116704002ef3b8e4342f1855cc48eb501c70d38
Snithya02 <167056142+Snithya02@users.noreply.github.com>
2024-04-29 19:50:48 +0530

    Add files via upload

---
+++ vowel2range.py
@@ -0,0 +1,17 @@
+'''a=input()
+if a in 'aeiouAEIOU':
+    print('vowel')
+else:
+    print('not an vowel')'''
+'''s=range(10)
+for i in s:
+    print(i,end=" ")'''
+'''s=range(10)
+for i in range(10,0,-1):
+    print(i,end=" ")'''
+'''s=range(10)
+print(list(s))'''
+s=range(1,10,1)
+print(list(s))
+s=range(1,10,1)
+print(s,type(s))
