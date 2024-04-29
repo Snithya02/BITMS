c8d23e3cf49f771d2c34113aa2f254f8e1688e94
Snithya02 <167056142+Snithya02@users.noreply.github.com>
2024-04-29 19:49:55 +0530

    Add files via upload

---
+++ userstring.py
@@ -0,0 +1,14 @@
+'''a=input()
+for i in a:
+    if i in 'aeiouAEIOU':
+      print(i,end=" ")'''
+'''g='abcd'
+for i in range(4):
+    print(i,g[i])'''
+s='abcd efgh ijkl'
+i=0
+while i<len(s) and s[i]!=" ":
+    print(s[i])
+    i+=1
+
+
