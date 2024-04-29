7e353b7ce1854d40c7abbb4f50e952de5ecb7bc7
Snithya02 <167056142+Snithya02@users.noreply.github.com>
2024-04-29 19:51:13 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,15 @@
+a=input()
+match a:
+    case 'a':
+        print("vowel")
+    case 'e':
+        print("vowel")
+    case 'i':
+        print(" vowel")
+    case 'o':
+        print("vowel")
+    case 'u':
+        print("vowel")
+    case _:
+        print("consonent")
+
